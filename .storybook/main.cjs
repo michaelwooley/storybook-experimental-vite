436a64217eb3ec32a6d3dbc552445e430e51de6a
Michael Wooley <wm.wooley@gmail.com>
2022-06-30 02:50:20 -0400

    clean up main.cjs

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,40 +1,13 @@
-// module.exports = {
-// 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
-// 	addons: [
-// 		'@storybook/addon-links',
-// 		'@storybook/addon-essentials',
-// 		'@storybook/addon-interactions'
-// 	],
-// 	framework: '@storybook/svelte',
-// 	core: {
-// 		builder: '@storybook/builder-vite'
-// 	},
-// 	svelteOptions: {
-// 		// "preprocess": require("../svelte.config.js").preprocess
-// 	},
-// 	features: {
-// 		storyStoreV7: true
-// 	},
-// 	typescript: {}
-// };
-
-// .storybook/main.ts
-
-// import type { StorybookViteConfig } from '@storybook/builder-vite';
-
-// const config =
 const preprocess = require('svelte-preprocess');
 const path = require('path');
-// const viteConfig = require('../vite.config');
 
-const { mergeConfig } = require('vite');
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'],
 	addons: [
 		'@storybook/addon-links',
 		'@storybook/addon-essentials',
-		'@storybook/addon-interactions',
-		'@storybook/addon-svelte-csf'
+		'@storybook/addon-interactions'
+		// '@storybook/addon-svelte-csf' // Expected to work?
 	],
 	framework: '@storybook/svelte',
 	core: {
@@ -49,46 +22,21 @@ module.exports = {
 		storyStoreV7: false
 	},
 	svelteOptions: {
-		preprocess: preprocess(),
-		// Possible with @sveltejs/vite-plugin-svelte version 1.0.0-next.43 or higher.
-		// Focus a story iframe and press cmd+shift (mac) or ctrl+shift (windows) to activate.
+		preprocess: preprocess(), // necessary to work currently. But does that mean that other stuff is ignored?
 		experimental: { inspector: false }
 	},
-	// async viteFinal(config, options) {
-	// 	console.log(config.plugins);
-	// 	config.plugins = config.plugins.concat(viteConfig.config);
-	// 	config.resolve.alias = {
-	// 		...(config.resolve.alias || {}),
-	// 		...{
-	// 			// $app:
-	// 			// 	//  path.resolve(__dirname, '../node_modules/@sveltejs/kit/assets/runtime/app')
-	// 			// 	path.resolve(__dirname, '../.svelte-kit/runtime/app')
 
-	// $app: path.resolve('./.svelte-kit/runtime/app'),
-	// $src: path.resolve('./src')
-	// 		}
-	// 	};
-	// 	// Allow files outside root for any aliased modules
-	// 	config.server.fs.strict = false;
-	// 	console.log(config.resolve);
-
-	// 	return config;
-	// }
 	async viteFinal(config) {
-		const c = (await import('../vite.config.js')).default;
-		console.log(c.resolve);
-
 		config.resolve.alias = {
 			...(config.resolve.alias || {}),
 			...{
-				$app: path.resolve('./.svelte-kit/runtime/app')
+				$app: path.resolve('./.svelte-kit/runtime/app'),
+				$lib: path.resolve('./src/lib')
 			}
 		};
 		config.server.fs.strict = false;
+		config.server.fs.allow = ['.'];
 		// Merge custom configuration into the default config
-		return mergeConfig(config, {
-			// Use the same "resolve" configuration as your app
-			resolve: (await import('../vite.config.js')).default.resolve
-		});
+		return config;
 	}
 };
